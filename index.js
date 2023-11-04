7565303c6a535868bd56d06487c91bf0d76eba40
rhmunna143 <rhmunna19@gmail.com>
2023-11-04 21:38:52 +0600

    DB driver setup done

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+require('dotenv').config()
 
 const app = express()
 const port = process.env.PORT || 8050;
+
+// middlewares
+app.use(express.json())
+app.use(cors())
+app.use(cookieParser())
+
+
 
 
 app.get("/", (req, res) => {
@@ -11,8 +22,43 @@ app.get("/", (req, res) => {
 
 
 
+// MongoDB driver
+
+// uri
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.52hv04l.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+// DB Functions
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        // await client.connect();
+        // Send a ping to confirm a successful connection
+        // await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+        // CRUD HERE
 
 
+        
+
+
+
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
 
 
 
